c0c1e44c94f82ec8bd37a498ff4e542da7eb07b6
Preetham404 <preethamshetty5353@gmail.com>
2026-03-17 21:33:44 +0530

    second commit

--- app.js
+++ app.js
@@ -68,6 +68,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
